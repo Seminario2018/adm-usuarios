bb799a5884e02cc06df40de3e776405c99523202
Seminario2018 <37277204+Seminario2018@users.noreply.github.com>
2018-03-13 17:22:02 -0300

    Add files via upload

---
+++ VentanaPrincipal.java
@@ -0,0 +1,52 @@
+package usuarios.vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+/**
+ * @author Martín Tomás Juran
+ * @version 1.0, 12 de mar. de 2018
+ */
+public class VentanaPrincipal {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal window = new VentanaPrincipal();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaPrincipal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new VistaUsuarios();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+	}
+
+}
---
+++ VistaAgregar.java
@@ -0,0 +1,47 @@
+package usuarios.vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Martín Tomás Juran
+ * @version 1.0, 13 de mar. de 2018
+ */
+public class VistaAgregar {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VistaAgregar window = new VistaAgregar();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VistaAgregar() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
